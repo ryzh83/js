b8d290d94ce6a3c99fe7177907840691020d34eb
Evgeniy <ya.ryzh83@yandex.ru>
2022-12-11 15:34:01 +0300

    циклы

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,29 @@ const personalMovieDb = {
   genres: [],
   privat: false,
 };
-const a = prompt("Какой последний фильм вы посмотрели?", "");
-const b = +prompt("На сколько оценили его?", "");
-personalMovieDb.movies[a] = b;
+if (personalMovieDb.count < 10) {
+  alert("мало фильмов");
+} else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
+  alert("хороший зритель");
+} else if (personalMovieDb.count >= 30) {
+  alert("много фильмов");
+} else {
+  alert("error");
+}
+// const a = prompt("Какой последний фильм вы посмотрели?", "");
+// b = +prompt("На сколько оценили его?", "");
+// personalMovieDb.movies[a] = b;
+// console.log(personalMovieDb);
+
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Какой последний фильм вы посмотрели?", ""),
+    b = +prompt("На сколько оценили его?", "");
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDb.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+}
 console.log(personalMovieDb);
