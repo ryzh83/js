ab9874f6c9a05dd3e14db5ecc5d18bd312a14898
Evgeniy <ya.ryzh83@yandex.ru>
2022-12-15 21:22:15 +0300

    obj

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// const { builtinModules } = require("module");
+
 let nunberOfFilms;
 
 function start() {
@@ -18,27 +20,53 @@ const personalMovieDb = {
   actors: {},
   genres: [],
   privat: true,
+  writeYourGENRES: function () {
+    for (let i = 1; i <= 3; i++) {
+      let genres = prompt(`Ваш любимый жанр ${i}`);
+      if (genres == null || genres == "") {
+        i--;
+      } else {
+        personalMovieDb.genres[i - 1] = genres;
+      }
+    }
+    personalMovieDb.genres.forEach((item, i) => {
+      console.log(`Любимый жанр ${i + 1} - это ${item}`);
+    });
+  },
+  toggleVizibleMyDB: function () {
+    if (personalMovieDb.privat) {
+      personalMovieDb.privat = false;
+    } else {
+      personalMovieDb.privat = true;
+    }
+  },
 };
-// let c;
-// let d;
-// let e;
-// function writeYourGenre() {
-//   c = prompt("Ваш любимый жанр 1", "");
-//   d = prompt("Ваш любимый жанр 2", "");
-//   e = prompt("Ваш любимый жанр 3", "");
-//   personalMovieDb.genres[0] = c;
-//   personalMovieDb.genres[1] = d;
-//   personalMovieDb.genres[2] = e;
-//   console.log(personalMovieDb.genres);
-// }
+
+personalMovieDb.toggleVizibleMyDB();
+console.log(personalMovieDb.privat);
+
+personalMovieDb.writeYourGENRES();
+
+let c;
+let d;
+let e;
+function writeYourGenre() {
+  c = prompt("Ваш любимый жанр 1", "");
+  d = prompt("Ваш любимый жанр 2", "");
+  e = prompt("Ваш любимый жанр 3", "");
+  personalMovieDb.genres[0] = c;
+  personalMovieDb.genres[1] = d;
+  personalMovieDb.genres[2] = e;
+  console.log(personalMovieDb.genres);
+}
 // writeYourGenre();
 
-function writeYourGENRES() {
-  for (let i = 1; i <= 3; i++) {
-    personalMovieDb.genres[i - 1] = prompt(`Ваш любимый жанр ${i}`);
-  }
-}
-writeYourGENRES();
+// function writeYourGENRES() {
+//   for (let i = 1; i <= 3; i++) {
+//     personalMovieDb.genres[i - 1] = prompt(`Ваш любимый жанр ${i}`);
+//   }
+// }
+// writeYourGENRES();
 
 // let c = 12;
 
@@ -85,6 +113,33 @@ function showMyDB(argument) {
     console.log(5);
   }
 }
-showMyDB(personalMovieDb.privat);
+// showMyDB(personalMovieDb.privat);
 
-console.log(personalMovieDb);
+// console.log(personalMovieDb);
+
+// const options = {
+//   with: 180,
+//   name: 'era',
+//   color: {
+//     border: 'red',
+//     padding: "blue"
+//   }
+// };
+
+// console.log(Object.keys(options.with).length);
+
+// const anr = {
+//    a: 1,
+//    b: 2,
+//    c: {
+//     d: 3,
+//     e: 4
+//    },
+// };
+
+// const bnr = {...anr};
+
+// bnr.a = 10;
+
+// console.log(anr);
+// console.log(bnr);
